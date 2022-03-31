f2bc82b56ddbdbf53461edd46ddf509d5e910c2e
jeje-01350 <jeremyrossi1234@gmail.com>
2022-03-31 20:16:05 +0200

    first

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 import 'jquery'
+import Typed from "typed.js";
 import '../bootstrap';
 import AOS from 'aos';
 
 AOS.init();
+
 
 /*
  * Welcome to your app's main JavaScript file!
@@ -10,9 +12,7 @@ AOS.init();
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-import '../js/alert';
-import '../js/progressbar';
-import '../js/nav-respons';
+import '../js/typedTitle';
 // any CSS you import will output into a single css file (app.css in this case)
 
 import '../styles/app.css';
---
+++ assets/js/typedTitle.js
@@ -0,0 +1,9 @@
+import Typed from "typed.js";
+
+let nomVariable = new Typed('.testos', {
+    strings: ["mes informations.","mes services.","mes expériences."], // phrase saffichant
+    typeSpeed: 100,
+    backSpeed: 50,
+    smartBackspace: true, // this is a default
+    loop: true
+});
