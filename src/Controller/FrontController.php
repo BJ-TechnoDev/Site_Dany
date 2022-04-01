3af04d87208e7b8b089152871d34d5022515e4aa
jeje-01350 <jeremyrossi1234@gmail.com>
2022-04-01 09:14:17 +0200

    footer

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
@@ -11,10 +15,33 @@ class FrontController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('')
+                ->subject($contactFormData['subject'])
+                ->text('Name: '.$contactFormData['fullName'].\PHP_EOL.'Subject: '.$contactFormData['subject'].\PHP_EOL.'Email: '.$contactFormData['email'].\PHP_EOL. 'Message: '.$contactFormData['message'],
+                    'text/plain');
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Your message has been sent');
+
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'our_form' => $form->createView()
         ]);
     }
 
