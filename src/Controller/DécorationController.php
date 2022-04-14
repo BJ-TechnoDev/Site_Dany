1937a395a81ebcf142b53ab477397fd4c6122f40
Sairkko <bastien.98@orange.fr>
2022-04-14 16:13:59 +0200

    Page deco / tab

---
+++ src/Controller/DécorationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DécorationController extends AbstractController
+{
+    /**
+     * @Route("/decoration", name="decoration")
+     */
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('bruyeredaniele01@gmail.com')
+                ->subject($contactFormData['subject'])
+                ->text('Name: '.$contactFormData['fullName'].\PHP_EOL.'Subject: '.$contactFormData['subject'].\PHP_EOL.'Email: '.$contactFormData['email'].\PHP_EOL. 'Message: '.$contactFormData['message'],
+                    'text/plain');
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Your message has been sent');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('front/decoration.html.twig', [
+            'our_form' => $form->createView()
+        ]);
+    }
+
+
+}
