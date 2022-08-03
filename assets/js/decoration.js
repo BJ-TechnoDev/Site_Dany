f6a8a89ed10614756d35c6f3375e9547852b8467
jeremy <jeremyrossi123@gmail.com>
2022-08-03 13:52:23 +0200

    title + metadescription

--- assets/js/decoration.js
+++ assets/js/decoration.js
@@ -1,1 +1,6 @@
 lightGallery(document.getElementById('lightgallery'))
+
+window.onload = function(){
+    document.getElementById("loader").style.visibility = "hidden";
+    document.getElementById("myDiv").style.visibility = "visible";
+}
